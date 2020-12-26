00de34523baf6c844ae8cbb90e363243b0611a51
igor.l <luchkoigor85@gmail.com>
2020-12-26 14:40:19 +0200

    issue with search string

--- src/components/RecentPosts/index.js
+++ src/components/RecentPosts/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState, useEffect} from 'react'
 import {useSelector, useDispatch} from "react-redux";
 import {Container, Row, Col} from 'react-grid-system';
 import './style.css';
@@ -28,40 +28,74 @@ const FeedRow = ({array}) => {
 };
 
 const findBySearchString = (publishedArticles, searchString) =>
-    publishedArticles.filter((el) =>
-        el.title.includes(searchString) || el.description.includes(searchString));
+    publishedArticles.filter(el =>
+        el.title.includes(searchString) ||
+        el.description.includes(searchString) ||
+        el.tagList.includes(searchString)
+    );
 
 const findBySearchTag = (publishedArticles, searchTag) =>
     publishedArticles.filter((el) =>
         el.tagList.includes(searchTag));
+
+const ResetButton = ({resetSearch}) => <div id="reset-centered">
+    <button
+        onClick={() => resetSearch()}
+        className="btn-switch"
+    >
+        Reset search
+    </button>
+</div>;
 
 const RecentPosts = (props) => {
     const dispatch = useDispatch();
     let publishedArticles = useSelector(getPublishedArticles);  // .slice(0, 3);
     const searchString = useSelector(getSearchString);
     const searchTag = useSelector(getSearchTag);
+    const [timer, setTimer] = useState(0);
+    const [noArticles, setNoArticles] = useState(false);
+    useEffect(() => {
+        const interval = setInterval(
+            () => {
+                setTimer((currentTimer) => currentTimer + 1)
+            },
+            1000
+        );
+        if (timer > 2) {
+            if (!publishedArticles.length) setNoArticles(true);
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval);
+    }, [timer, publishedArticles, searchString]);
+
+
     const resetSearch = () => {
-        dispatch(clearSearch())
+        dispatch(clearSearch());
+        setNoArticles(false);
     };
 
     if (searchString) publishedArticles = findBySearchString(publishedArticles, searchString);
     if (searchTag) publishedArticles = findBySearchTag(publishedArticles, searchTag);
-
+    if (noArticles && searchString) {
+        return <Fragment>
+            <div id="reset-centered">
+                <h2>No articles found(=</h2>
+            </div>
+            <ResetButton
+                resetSearch={resetSearch}
+            />
+        </Fragment>
+    }
     return publishedArticles.length ?
         (<Fragment>
             {
                 (searchString || searchTag) &&
-                <div id="reset-centered">
-                    <button
-                        onClick={() => resetSearch()}
-                        className="btn-switch"
-                    >
-                        Reset search
-                    </button>
-                </div>
+                <ResetButton
+                    resetSearch={resetSearch}
+                />
             }
             {splitEvery(publishedArticles, 3).map((articlesRowChunk, index) =>
-                <FeedRow array={articlesRowChunk} key={index} />)
+                <FeedRow array={articlesRowChunk} key={index}/>)
             }
         </Fragment>) : <Preloader/>
 };
