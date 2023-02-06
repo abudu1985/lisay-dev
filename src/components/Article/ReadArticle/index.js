0a6de7c9717c2c5a8dfd19797d66f6d62d283046
igor.l <luchkoigor85@gmail.com>
2023-02-06 16:30:03 +0200

    commit last changes

--- src/components/Article/ReadArticle/index.js
+++ src/components/Article/ReadArticle/index.js
@@ -1,67 +1,78 @@
-import React, {useEffect} from 'react'
-import {connect, useSelector} from "react-redux";
-import {useHistory} from 'react-router-dom';
-import {branch, compose, lifecycle, pure, renderComponent, withProps, withState} from 'recompose';
+import React, { useEffect } from "react";
+import { connect, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import {
+  branch,
+  compose,
+  lifecycle,
+  pure,
+  renderComponent,
+  withProps,
+  withState,
+} from "recompose";
 
-import './style.css';
 import withGlobalLayout from "../../hoc/withGlobalLayout";
-import {getSearchString, getSearchTag} from "../../../store/selectors/searchSelectors";
+import {
+  getSearchString,
+  getSearchTag,
+} from "../../../store/selectors/searchSelectors";
 import { getArticles } from "../../../store/selectors/articlesSelectors";
 import ArticleBodyBlock from "./ArdicleBodyBlock";
 import Preloader from "../../Preloader";
 import NotFound from "../../NotFound";
 
+import "./style.css";
+
 const scrollWindowToTop = () => window.scrollTo(0, 0);
 
-const ReadArticle = ({article,seconds}) => {
-    const searchString = useSelector(getSearchString);
-    const searchTag = useSelector(getSearchTag);
-    const history = useHistory();
+const ReadArticle = ({ article, seconds }) => {
+  const searchString = useSelector(getSearchString);
+  const searchTag = useSelector(getSearchTag);
+  const history = useHistory();
 
-    useEffect(() => {
-    if (searchString.trim().length || searchTag.trim().length) {
-            history.push({
-                pathname: `/`,
-                state: {}
-            })
-        }
-    }, [article, searchTag, searchString, seconds]);
-
+  useEffect(() => {
     scrollWindowToTop();
-    return article ? <ArticleBodyBlock article={article}/> : <NotFound />
+  }, [article]);
 
+  useEffect(() => {
+    if (searchString.trim().length || searchTag.trim().length) {
+      history.push({
+        pathname: `/`,
+        state: {},
+      });
+    }
+  }, [searchTag, searchString, seconds]);
+
+  return article ? <ArticleBodyBlock article={article} /> : <NotFound />;
 };
 
-const mapStateToProps = state => {
-    return {
-        articles: getArticles(state)
-    };
+const mapStateToProps = (state) => {
+  return {
+    articles: getArticles(state),
+  };
 };
 
 export default compose(
-    connect(mapStateToProps),
-    withProps(({articles, location}) => {
-        const currentPath = location.pathname;
-        let slug = currentPath.replace('/article/' , '');
-        const article = articles.find(ar => ar.slug === slug);
-        return {article};
-    }),
-    withState('seconds', 'updateSeconds', 0),
-    lifecycle({
-        componentDidMount() {
-            const { seconds, updateSeconds} = this.props;
-            setTimeout(
-                () => {
-                    updateSeconds(seconds + 1);
-                },
-                4000,
-            );
-        }
-    }),
-    pure,
-    branch(
+  connect(mapStateToProps),
+  withProps(({ articles, location }) => {
+    const currentPath = location.pathname;
+    let slug = currentPath.replace("/article/", "");
+    const article = articles.find((ar) => ar.slug === slug);
+    return { article };
+  }),
+  withState("seconds", "updateSeconds", 0),
+  lifecycle({
+    componentDidMount() {
+      const { seconds, updateSeconds } = this.props;
+      setTimeout(() => {
+        updateSeconds(seconds + 1);
+      }, 4000);
+    },
+  }),
+  pure,
+  branch(
     ({ article, seconds }) => !article && !seconds,
     renderComponent(Preloader)
-    ),
-    withGlobalLayout
+  ),
+  withGlobalLayout
 )(ReadArticle);
